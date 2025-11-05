8bfef6e89f92b312eafe69c5f22d368984fc2d86
Eurico M. <eurico.dp.magalhaes@outlook.com>
2025-11-05 09:45:27 +0000

    casa

--- aula_05/PC021.cpp
+++ aula_05/PC021.cpp
@@ -1,3 +1,7 @@
+// Construir a matriz de somas cumulativas.
+// Fixar duas linhas da matriz, e usar Kadane como se as colunas delimitadas por essas
+// linhas fossem elementos únicos (a soma da coluna).
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -15,19 +19,22 @@ void print_matrix(vector<vector<int>> m, int size) {
     }
 }
 
-int kadane(vector<vector<int>> sums, int size, int pos_i, int pos_j) {
-    int max = -101;
+// soma de uma coluna j, desde i_start até i_end
+int column(vector<vector<int>> sums, int i_start, int i_end, int j) {
+    return sums[i_end][j] - sums[i_start-1][j] - sums[i_end][j-1] + sums[i_start-1][j-1];
+}
 
-    for (int i = 1; i <= pos_i; i++) {
-        for (int j = 1; j <= pos_j; j++) {
-            int sum = sums[pos_i][pos_j] - sums[i-1][pos_j] - sums[pos_i][j-1] + sums[i-1][j-1];
-            if (sum > max) {
-                max = sum;
-            }
-        }
+// kadane numa dimensão 'n', delimitada por duas linhas 'a' e 'b'
+int kadane(vector<vector<int>> sums, int size, int a, int b) {
+    int current_sum = column(sums, a, b, 1);
+    int best_sum = current_sum;
+
+    for (int n = 2; n <= size; n++) {
+        current_sum = max(column(sums, a, b, n), current_sum + column(sums, a, b, n));
+        best_sum = max(best_sum, current_sum);
     }
 
-    return max;
+    return best_sum;
 }
 
 
@@ -41,14 +48,14 @@ int main() {
     cin >> n;
 
     // construir matriz e matriz de somas acumuladas
-    vector<vector<int>> matrix(n + 1, vector<int>(n + 1, 0));
+    //vector<vector<int>> matrix(n + 1, vector<int>(n + 1, 0));
     vector<vector<int>> cumSums(n + 1, vector<int>(n + 1, 0));
 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
             int num;
             cin >> num;
-            matrix[i][j] = num;
+            //matrix[i][j] = num;
             cumSums[i][j] = num + cumSums[i][j-1];
         }
     }
@@ -59,18 +66,17 @@ int main() {
         }
     }
 
-    cout << "Matrix:\n";
-    print_matrix(matrix, n);
-    cout << "\nCumSums:\n";
-    print_matrix(cumSums, n);
+    // cout << "Matrix:\n";
+    // print_matrix(matrix, n);
+    // cout << "\nCumSums:\n";
+    // print_matrix(cumSums, n);
 
     int max_sum = -101;
 
-    // calcular para cada elemento da matriz de somas acumuladas
-    // a maior soma
-    for (int i = 1; i <= n; i++) {
-        for (int j = i; j <= n; j++) {
-            int sum_at = kadane(cumSums, n, i, j);
+    // fixar linhas (a e b) e usar Kadane nessa dimensão
+    for (int a = 1; a <= n; a++) {
+        for (int b = a; b <= n; b++) {
+            int sum_at = kadane(cumSums, n, a, b);
             if (sum_at > max_sum) {
                 max_sum = sum_at;
             }
