c44378c9ee96b9c87e6ae354003333ef5002c5c2
Eurico M. <eurico.dp.magalhaes@outlook.com>
2025-11-12 13:27:32 +0000

    casa

--- aula_06/PC026.cpp
+++ aula_06/PC026.cpp
@@ -1,9 +1,15 @@
 #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 
-#define INF -1000001
+#define INF 1000001
+#define OFFSET 1000
 
-void print_vector(vector<int> v) {
+vector<int> alice_seq;
+vector<int> bob_seq;
+vector<vector<int>> dp;
+
+void print_vector(vector<int> &v) {
     cout << "[";
     for (auto x : v) {
         cout << " " << x;
@@ -11,20 +17,39 @@ void print_vector(vector<int> v) {
     cout << " ]\n";
 }
 
-void print_matrix(vector<vector<int>> m) {
+void print_matrix(vector<vector<int>> &m) {
     for (auto x: m) {
         print_vector(x);
     }
 }
 
 void receive_seq(vector<int> &seq, int sz) {
-    for (int i = 0; i < sz; i++) {
+    seq.push_back(-INF);
+    for (int i = 1; i <= sz; i++) {
         int num;
         cin >> num;
         seq.push_back(num);
     }
 }
 
+int lcs_rec(int i, int j) {
+    if (i == 0 || j == 0) {
+        return -INF;
+    }
+
+    if (dp[i][j] != -INF) {
+        return dp[i][j];
+    }
+
+    int use_both;
+    if (lcs_rec(i-1, j-1) == -INF) {
+        use_both = alice_seq[i] * bob_seq[j];
+    } else {
+        use_both = alice_seq[i] * bob_seq[j] + lcs_rec(i-1, j-1);
+    }
+
+    return dp[i][j] = max(use_both, max(lcs_rec(i, j-1), lcs_rec(i-1,j)));
+}
 
 int main() {
     // Fast IO
@@ -34,42 +59,21 @@ int main() {
 
     int n;
     cin >> n;
-    vector<int> alice_seq;
     receive_seq(alice_seq, n);
 
     int m;
     cin >> m;
-    vector<int> bob_seq;
     receive_seq(bob_seq, m);
+
+    dp.assign(n+1, vector<int> (m+1, -INF));
 
     // cout << "checking vectors:\n";
     // print_vector(alice_seq);
     // print_vector(bob_seq);
-
-    vector<vector<int>> dp (n+1, vector<int>(m+1, INF));
-
+    // cout << "dp:\n";
     // print_matrix(dp);
 
-    // longest common subsequence Bottom-Up DP (Tabulation) (geeks for geeks)
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            int multiplication = alice_seq[i-1] * bob_seq[j-1];
-            if (multiplication > 0 || multiplication > dp[i][j-1]) {
-                if (dp[i-1][j-1] < 0) {
-                    dp[i][j] = multiplication;
-                }
-                else {
-                    dp[i][j] = dp[i-1][j-1] + multiplication;
-                }
-            } else {
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-    }
-
-    //print_matrix(dp);
-
-    cout << dp[n][m] << "\n";
+    cout << lcs_rec(n, m) << "\n";
 
     return 0;
 }
