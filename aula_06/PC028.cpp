fd2972f39030912ec5a9f55b1c1108544b45ff84
Eurico M. <eurico.dp.magalhaes@outlook.com>
2025-11-15 11:38:19 +0000

    casa

--- aula_06/PC028.cpp
+++ aula_06/PC028.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 vector<int> seq;
-vector<int> dp;
+vector<int> dp_LR;
+vector<int> dp_RL;
 int n;
 
 void print_vector(vector<int> &v) {
@@ -23,33 +24,63 @@ void receive_numbers() {
 }
 
 void configure_dp() {
-    dp.clear();
-    dp.assign(n, -1);
-    dp[0] = 1;
+    dp_LR.clear();
+    dp_RL.clear();
+    dp_LR.assign(n, -1);
+    dp_RL.assign(n, -1);
 }
 
-int lis_rec(int idx) {
-    if (idx == 0 || dp[idx] != -1) {
-        return dp[idx];
+int lis_LR(int idx) {
+    if (idx == 0) {
+        return dp_LR[idx] = 1;
     }
 
-    int max_temp = 0;
+    if (dp_LR[idx] != -1) {
+        return dp_LR[idx];
+    }
+
+    int max_temp = 1;
     for (int i = 0; i < idx; i++) {
         if (seq[i] < seq[idx]) {
-            max_temp = max(max_temp, 1 + lis_rec(i));
+            max_temp = max(max_temp, 1 + lis_LR(i));
         }
     }
-    dp[idx] = max_temp;
+    dp_LR[idx] = max_temp;
+    return max_temp;
+}
+
+int lis_RL(int idx) {
+    if (idx == n-1) {
+        return dp_RL[idx] = 1;
+    }
+
+    if (dp_RL[idx] != -1) {
+        return dp_RL[idx];
+    }
+
+    int max_temp = 1;
+    for (int i = n-1; i > idx; i--) {
+        if (seq[i] < seq[idx]) {
+            max_temp = max(max_temp, 1 + lis_RL(i));
+        }
+    }
+    dp_RL[idx] = max_temp;
     return max_temp;
 }
 
 int lis() {
 
-    int res = -1;
+    // Percorrer todos os elementos da sequência e tratá-los como se fossem o 'ponto central'
+    // da wavio sequence: calcular a maior sequência válida à esquerda e à direita.
+    // O mínimo destes valores pode ser usado para construir uma sequência wavio com os valores
+    // do outro lado.
+    //
+    // O ponto central da maior sequência wavio será o que tiver o maior desses valores mínimos.
+    int max_min = 0;
     for (int i = 0; i < n; i++) {
-        res = max(res, lis_rec(i));
+        max_min = max(max_min, min(lis_LR(i), lis_RL(i)));
     }
-    return res;
+    return max_min * 2 - 1;
 }
 
 int main() {
@@ -66,9 +97,11 @@ int main() {
         receive_numbers();
         configure_dp();
 
+        // cout << "TEST #" << i + 1 << "\n";
+        // print_vector(seq);
         cout << lis() << "\n";
-        print_vector(seq);
-        print_vector(dp);
+        // print_vector(dp_LR);
+        // print_vector(dp_RL);
     }
 
     return 0;
